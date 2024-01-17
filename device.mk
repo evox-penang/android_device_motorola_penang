230efb5ad0f1b9b4853306a25f517a1b61f3374d
lahaina <nattolecats@gmail.com>
2024-01-17 19:23:55 +0900

    penang: Import perf configs * Too much SystemUI lagging on XT2335-4

--- device.mk
+++ device.mk
@@ -75,6 +75,11 @@ PRODUCT_PACKAGES += \
     penangFrameworksOverlaySB \
     penangSettingsProviderOverlaySB
 
+# Perf
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json
+
 # GCamGo
 PRODUCT_PACKAGES += \
     GCamGOPrebuilt-V4
